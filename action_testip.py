e11e83b6b07516714436b0afa0b6202b84af6504
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2025-02-28 16:11:36 +0800

    Update action_testip.py

--- action_testip.py
+++ action_testip.py
@@ -1,5 +1,6 @@
 import requests
-import logging
+import logging,sys
+sys.stdout.reconfigure(encoding="utf-8")  # Python 3.7 及以上适用
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
